ff7a6e1376344a682d64ba65065fbb48d855a697
sanjivkumar <114769026+sanjivks@users.noreply.github.com>
2023-04-24 19:35:33 +0530

    Add files via upload

---
+++ services.js
@@ -0,0 +1,44 @@
+var Services = {
+    Save_Contect(obj) {
+        var btnstatus = $("#btnadd").val();
+
+        if (btnstatus == "ADD") {
+            itemJsonArry.push(obj);
+        }
+        else if (btnstatus == "UPDATE") {
+
+            let itemindex = $("#btnHid").val();
+            itemJsonArry[itemindex] = obj;
+        }
+
+        localStorage.setItem('itemsJson', JSON.stringify(itemJsonArry))
+        update();
+
+    },
+
+
+    get_Contect(obj) {
+        let selectedElementIndex = $('.select-body').index($(obj));
+        return selectedElementIndex;
+    },
+
+    // edit_Data(obj){
+    //     let itemdata =itemJsonArry[obj];
+    //     return itemdata;
+    // },
+
+
+
+    delete_Data(itemindex) {
+
+        itemJsonArry.splice(itemindex, 1);
+        localStorage.setItem('itemsJson', JSON.stringify(itemJsonArry))
+        update();
+
+    }
+
+
+
+
+
+}
